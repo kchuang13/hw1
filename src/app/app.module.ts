0569209ba545fe9891bb2f5677388edc77c23449
Kelly Chuang <kchuang95@yahoo.com>
2020-10-19 18:31:31 -0500

    added travel form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TravelComponent } from './travel/travel.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TravelComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, TravelComponent]
 })
 export class AppModule { }
