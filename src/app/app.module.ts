dc85724b9a917e6eee752d40017ca2d029986ac6
Kelly Chuang <kchuang95@yahoo.com>
2020-11-02 19:08:01 -0600

    added routing and about component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { TravelComponent } from './travel/travel.component';
 import { FormsModule } from '@angular/forms';
+import { AboutComponent } from './about/about.component';
+
+const routes: Routes = [
+  {path: 'travel', component:TravelComponent},
+  {path: 'about', component:AboutComponent},
+  {path: '', redirectTo: '/about', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    TravelComponent
+    TravelComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
-  bootstrap: [AppComponent, TravelComponent]
+  bootstrap: [AppComponent, TravelComponent, AboutComponent]
 })
 export class AppModule { }
